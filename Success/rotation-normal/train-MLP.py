905e39edd67cfb882b2f7b7e8a1e76cc74b5891a
a-green-hand-jack <green-hand-jack@users.noreply.github.com>
2023-10-19 21:56:42 +0800

    成功解决了旋转中的问题，也就是之前没有得到预期的先上升后下降；参考原文，在-30°到+30°之间以1°为梯度进行考察，得到了准确率的变化情况；符合预期。

---
+++ Success/rotation-normal/train-MLP.py
@@ -0,0 +1,54 @@
+# %%
+# 加载各种库
+import torch
+import os 
+import torchvision
+from torchvision import datasets, transforms
+import torch.multiprocessing as mp
+import pickle
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import random
+from torch.optim.lr_scheduler import CosineAnnealingLR  # 实现cos函数式的变化
+from tqdm import tqdm
+
+# 加载支持函数和类
+from support_code.load_dataset import get_data_loader
+from support_code.LeNet import LeNet
+from support_code.get_scale import get_scale
+from support_code.MLP import MLP
+
+# 设置设备和种子
+device_cuda = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# %%
+# 测试新的方法
+
+
+if __name__ == '__main__':
+
+  
+  i = 2
+  min_angle_list = range(-30,1,1)
+  # for min_angle in tqdm(min_angle_list, unit="degree", desc="min_angle"):
+  for min_angle in min_angle_list:
+    max_angle = -min_angle
+
+    # print("------------------开始新的循环-----------\nmin_angle={},max_angle={}".format(min_angle, max_angle))
+    augment = transforms.RandomRotation(degrees=(min_angle, max_angle))
+    # augment = None
+    train_loader, test_loader = get_data_loader(min_angle=min_angle, max_angle=max_angle, RandAugment=augment)
+    get_scale(
+      train_loader=train_loader, 
+      test_loader=test_loader,
+      path = "MLP-30-30", 
+      num_epochs=80,
+      net=MLP(),
+      min_angle=min_angle,
+      max_angle=max_angle,
+      batch_size_train=64,
+      batch_size_test=64,
+      number=i)
+    i += 1
+    
